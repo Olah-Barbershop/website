5f8fa041c38c728caa8f733bd098dafcd6076954
pml68 <tutorialmester@gmail.com>
2024-01-10 21:29:35 +0100

    feat: Contact info is now retrieved from API, but letter effects aren't working(yet)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,40 +3,56 @@
   import '$lib/assets/css/contact.scss'
 
   const letterise = elclass => {
-    const element = document.querySelector(elclass),
-          text = element.innerText.split("")
+    const elements = document.querySelectorAll(elclass)
 
-    element.innerText = ""
+    for(const element of elements) {
+      const text = element.innerText.split("")
 
-    text.forEach(letter => {
-      const span = document.createElement('span')
+      element.innerText = ""
 
-      span.className = "letter"
-      span.innerText = letter
+      text.forEach(letter => {
+        const span = document.createElement('span')
 
-      element.appendChild(span)
-    })
+        span.className = "letter"
+        span.innerText = letter
+
+        element.appendChild(span)
+      })
+    }
   }
 
-  onMount(() => {
-    letterise(".email")
-    letterise(".insta")
-    letterise(".threads")
-    letterise(".dev")
+  type Contact = {
+    name: string,
+    link: string
+  }
+
+  type Line = {
+    /**
+     * Left contact
+     */
+    left: Contact,
+    right: Contact
+  }
+
+  let lines: Line[]
+  let error: Error
+
+  onMount(async () => {
+    try {
+      const results = await fetch('https://olahbarbershop.codes/contacts')
+      if(!results.ok) {
+        throw new Error('Network response was not OK.')
+      }
+      lines = await results.json()
+    } catch (err) {
+      error = err
+    }
   })
-</script>
+
+  </script>
 
  <div class="text">
-    <div class="line">
-        <p class="word normal">This</p>
-        <p class="word normal">Is</p>
-        <p class="word normal">Your</p>
-    </div>
-    <div class="line">
-        <p class="word normal">Barbershop</p>
-        <p class="word normal">▼</p>
-    </div>
-    <div class="line">
+        <!--div class="line">
       <a href="mailto:contact@olahbarbershop.codes" target="_blank">
         <p class="fancy word email">Email</p>
       </a>
@@ -51,5 +67,53 @@
       <a href="https://dev.to/olahbarbershop" target="_blank">
         <p class="fancy word dev">DEV</p>
       </a>
-    </div>
+    </div-->
+    {#if lines}
+      <div class="line">
+        <p class="word normal">This</p>
+        <p class="word normal">Is</p>
+        <p class="word normal">Your</p>
+      </div>
+      <div class="line">
+        <p class="word normal">Barbershop</p>
+        <p class="word normal">▼</p>
+      </div>
+      {#each lines as { left, right }}
+        <div class="line">
+          <a href={left.link} target="_blank">
+            <p class={"fancy word " + left.name}>{left.name}</p>
+          </a>
+          <a href={right.link} target="_blank">
+            <p class={"fancy word " + right.name}>{right.name}</p>
+          </a>
+        </div>
+      {/each}
+      {void letterise('.fancy') ?? ""}
+    {:else if error}
+      <p>{error.message}</p>
+    {:else}
+      <p>Loading...</p>
+    {/if}
+    <script>
+    const letterise = elclass => {
+    const elements = document.querySelectorAll(elclass)
+
+    for(const element of elements) {
+      const text = element.innerText.split("")
+
+      element.innerText = ""
+
+      text.forEach(letter => {
+        const span = document.createElement('span')
+
+        span.className = "letter"
+        span.innerText = letter
+
+        element.appendChild(span)
+      })
+    }
+  }
+
+      letterise('.fancy')
+    </script>
 </div>
