1023498a2400894c8130ef9daf8c903e0b459cdb
pml68 <tutorialmester@gmail.com>
2024-01-14 19:04:17 +0100

    feat: add something to the homepage (although not much, I have no ideas for it)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+  import '$lib/assets/css/home.scss'
+</script>
+
 <svelte:head>
   <title>Oláh Barbershop</title>
 </svelte:head>
 
-<div class="ii"></div>
+<div class="container">
+  <div class="main">
+    <h1>Hi!</h1>
+    <br>
+    <h1>This is <b>your</b> barbershop.</h1>
+  </div> 
+</div>
